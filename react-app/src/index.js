e1c606496f3f134d85bb8683b28a0fdecfcf3814
andyrose507 <drew.jones1998@gmail.com>
2022-01-19 14:48:44 -0700

    navbar spacing

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import GlobalStyle from './theme/GlobalStyle'
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Provider store={store}>
         <App />
       </Provider>
